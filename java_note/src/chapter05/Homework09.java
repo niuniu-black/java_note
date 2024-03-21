ca92bc2626c2c1dbad4f259ceb4383a9dc460998
lrx <aaa@aa.com>
2024-03-21 23:58:43 +0800

    fixed chapter05/Homework09.java bug

--- java_note/src/chapter05/Homework09.java
+++ java_note/src/chapter05/Homework09.java
@@ -10,7 +10,7 @@ public class Homework09 {
 
         int sum = 0;
 
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             for (int j = 1; j <= i; j++) {
                 sum += j;
 //                System.out.print(j + " ");
